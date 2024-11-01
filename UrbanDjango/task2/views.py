2dfd11e335ba6d23ebbc0dfcf805e85b90926648
OlegMSU56 <sagdeev.ol@yandex.ru>
2024-11-02 01:56:09 +0300

    02/11/24 task2

---
+++ UrbanDjango/task2/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+# Create your views here.
+def function_view(request):
+    return render(request, 'second_task/function_template.html')
+
+class class_view(TemplateView):
+    template_name = 'second_task/class_template.html'
